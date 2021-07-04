8e87d1d2940da5a39aca005261024f9e9ed86bfb
Abrar <abetrack3@yahoo.com>
2021-07-05 01:11:49 +0600

    Reorganised the code and Implemented the Extraction of Date of Speech delivered

--- src/main/java/CodeRunner.java
+++ src/main/java/CodeRunner.java
@@ -1,8 +1,15 @@
-import java.io.IOException;
+import GovDotUk.SpeechLinkScraper;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class CodeRunner {
-    public static void main(String[] args) throws IOException {
-//        LearnerSpace.MyScraper.main(null);
-        ScraperDevelopment.SpeechLinkScraper.main(null);
+    public static void main(String[] args) {
+        String name = "Theresa May";
+        new SpeechLinkScraper("Theresa May", 1, 100);
     }
+
 }
